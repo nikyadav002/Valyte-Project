e073e4b22bb3be90a62478771730fb244d947302
Nikhil Singh <nikhil002.iitd@gmail.com>
2026-06-23 18:19:40 +0530

    bump: version 0.24.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="valyte",
-    version="0.23.0",
+    version="0.24.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
